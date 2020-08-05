d48a5c3ba5760eb0e5fb5276d81392ef3d04dfc8
Stephen Lee <stephen_lee_94@hotmail.com>
2020-08-04 23:42:21 -0700

    find_board and get_state fxns

--- test.py
+++ test.py
@@ -19,16 +19,30 @@ TILES = {
     'M': Image.open(f'{IMGS}/mine.png'),
 }
 
-# obtain coordinates for Minesweeper board
-modes = ['beginner', 'intermediate', 'expert']
-boards = [pg.locateOnScreen(f'{IMGS}/{mode}.png') for mode in modes]
-for x in boards:
-    if x != None:
-        board = x
+def find_board():
+    # obtain coordinates for Minesweeper board
+    modes = ['beginner', 'intermediate', 'expert']
+    boards = [pg.locateOnScreen(f'{IMGS}/{mode}.png') for mode in modes]
+    for x in boards:
+        if x != None:
+            board = x
 
-unsolved_tile = Image.open(f'{IMGS}/unsolved.png')
-tiles = pg.locateAllOnScreen(unsolved_tile)
-tiles = list(tiles)
+    assert board != None, 'Minesweeper board not detected on screen'
+    return board
+
+def get_state(bbox):
+    all_tiles = [[t, list(pg.locateAllOnScreen(TILES[t], region=bbox))] for t in TILES]
+
+    tiles = []
+    for value, coords in all_tiles:
+        for coord in coords:
+            tiles.append({'coord': coord, 'value': value})
+    tiles = sorted(tiles, key=lambda x: (x['coord'][1], x['coord'][0]))
+
+    height = sum(1 for i in tiles if i['coord'][0]==tiles[0]['coord'][0])
+    width = sum(1 for i in tiles if i['coord'][1]==tiles[0]['coord'][1])
+
+    current_board = {i: {'coord': tile['coord'], 'value': tile['value'], 'action': None} for i, tile in enumerate(tiles)}
 
 x,y = tiles[0][0], tiles[0][1]
 
