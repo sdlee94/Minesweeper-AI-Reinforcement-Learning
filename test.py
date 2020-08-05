c253ad2130551a6d9d17598910678f3b49cc9775
Stephen Lee <stephen_lee_94@hotmail.com>
2020-08-05 08:55:34 -0700

    Minesweeper class

--- test.py
+++ test.py
@@ -19,30 +19,38 @@ TILES = {
     'M': Image.open(f'{IMGS}/mine.png'),
 }
 
-def find_board():
-    # obtain coordinates for Minesweeper board
-    modes = ['beginner', 'intermediate', 'expert']
-    boards = [pg.locateOnScreen(f'{IMGS}/{mode}.png') for mode in modes]
-    for x in boards:
-        if x != None:
-            board = x
+class Minesweeper:
+    def __init__(self, TILES):
+        self.TILES = TILES
+        self.loc = self.find_board()
+        self.state = self.get_state(self.loc)
 
-    assert board != None, 'Minesweeper board not detected on screen'
-    return board
+    def find_board(self):
+        # obtain coordinates for Minesweeper board
+        modes = ['beginner', 'intermediate', 'expert']
+        boards = [pg.locateOnScreen(f'{IMGS}/{mode}.png') for mode in modes]
 
-def get_state(bbox):
-    all_tiles = [[t, list(pg.locateAllOnScreen(TILES[t], region=bbox))] for t in TILES]
+        assert boards != [None, None, None], 'Minesweeper board not detected on screen'
 
-    tiles = []
-    for value, coords in all_tiles:
-        for coord in coords:
-            tiles.append({'coord': coord, 'value': value})
-    tiles = sorted(tiles, key=lambda x: (x['coord'][1], x['coord'][0]))
+        for x in boards:
+            if x != None:
+                board = x
 
-    height = sum(1 for i in tiles if i['coord'][0]==tiles[0]['coord'][0])
-    width = sum(1 for i in tiles if i['coord'][1]==tiles[0]['coord'][1])
+        return board
 
-    current_board = {i: {'coord': tile['coord'], 'value': tile['value'], 'action': None} for i, tile in enumerate(tiles)}
+    def get_state(self, bbox):
+        all_tiles = [[t, list(pg.locateAllOnScreen(self.TILES[t], region=bbox))] for t in self.TILES]
+
+        tiles = []
+        for value, coords in all_tiles:
+            for coord in coords:
+                tiles.append({'coord': (coord[0], coord[1]), 'value': value})
+        tiles = sorted(tiles, key=lambda x: (x['coord'][1], x['coord'][0]))
+
+        self.height = sum(1 for i in tiles if i['coord'][0]==tiles[0]['coord'][0])
+        self.width = sum(1 for i in tiles if i['coord'][1]==tiles[0]['coord'][1])
+
+        return {i: {'coord': tile['coord'], 'value': tile['value'], 'action': None} for i, tile in enumerate(tiles)}
 
 x,y = tiles[0][0], tiles[0][1]
 
