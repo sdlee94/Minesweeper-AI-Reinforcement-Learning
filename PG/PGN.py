e563f580421f90b5c7ef4fa0e234c63a19432745
Stephen Lee <stephen_lee_94@hotmail.com>
2020-10-10 08:38:35 -0700

    policy gradient network

---
+++ PG/PGN.py
@@ -0,0 +1,18 @@
+from keras.layers import Conv2D, Dense
+from keras.models import load_model
+from keras.optimizers import Adam
+
+def create_policy_network(learn_rate, input_dims, n_actions, conv_units, dense_units):
+    model = Sequential([
+                Conv2D(conv_units, (3,3), activation='relu', padding='same', input_shape=input_dims),
+                Conv2D(conv_units, (3,3), activation='relu', padding='same'),
+                Conv2D(conv_units, (3,3), activation='relu', padding='same'),
+                Conv2D(conv_units, (3,3), activation='relu', padding='same'),
+                Flatten(),
+                Dense(dense_units, activation='relu'),
+                Dense(dense_units, activation='relu'),
+                Dense(n_actions, activation='softmax')])
+
+    model.compile(optimizer=Adam(lr=learn_rate), loss='categorical_crossentropy')
+
+    return model
