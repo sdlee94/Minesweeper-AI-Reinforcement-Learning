0c03964f2baaf0cac611957f4f644224a831f3be
Stephen Lee <stephen_lee_94@hotmail.com>
2020-10-16 10:29:27 -0700

    fix model name

--- DQN/train.py
+++ DQN/train.py
@@ -84,7 +84,7 @@ def main():
             with open(f'replay/{MODEL_NAME}.pkl', 'wb') as output:
                 pickle.dump(agent.replay_memory, output)
 
-            agent.model.save(f'models/{MODEL_NAME}_{best}.h5')
+            agent.model.save(f'models/{MODEL_NAME}.h5')
 
 if __name__ == "__main__":
     main()
